04e8bf3441040e368aa1f7b570c8144cabdb4c1f
erikatini <katini94@gmail.com>
2015-01-31 18:58:31 +0700

    Commit Kabinet Kerja

---
+++ src/com/eritrikat/kabinetkerjajokowi_jk/Halaman2.java
@@ -0,0 +1,173 @@
+package com.eritrikat.kabinetkerjajokowi_jk;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Halaman2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_halaman2);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.halaman2, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	public void openMenteri1 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri1.class);
+		startActivity(i);
+		}
+	public void openMenteri2 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri2.class);
+		startActivity(i);
+		}
+	public void openMenteri3 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri3.class);
+		startActivity(i);
+		}
+	public void openMenteri4 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri4.class);
+		startActivity(i);
+		}
+	public void openMenteri5 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri5.class);
+		startActivity(i);
+		}
+	public void openMenteri6 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri6.class);
+		startActivity(i);
+		}
+	public void openMenteri7 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri7.class);
+		startActivity(i);
+		}
+	public void openMenteri8 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri8.class);
+		startActivity(i);
+		}
+	public void openMenteri9 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri9.class);
+		startActivity(i);
+		}
+	public void openMenteri10 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri10.class);
+		startActivity(i);
+		}
+	public void openMenteri11 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri11.class);
+		startActivity(i);
+		}
+	public void openMenteri12 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri12.class);
+		startActivity(i);
+		}
+	public void openMenteri13 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri13.class);
+		startActivity(i);
+		}
+	public void openMenteri14 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri14.class);
+		startActivity(i);
+		}
+	public void openMenteri15 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri15.class);
+		startActivity(i);
+		}
+	public void openMenteri16 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri16.class);
+		startActivity(i);
+		}
+	public void openMenteri17 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri17.class);
+		startActivity(i);
+		}
+	public void openMenteri18 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri18.class);
+		startActivity(i);
+		}
+	public void openMenteri19 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri19.class);
+		startActivity(i);
+		}
+	public void openMenteri20 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri20.class);
+		startActivity(i);
+		}
+	public void openMenteri21 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri21.class);
+		startActivity(i);
+		}
+	public void openMenteri22 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri22.class);
+		startActivity(i);
+		}
+	public void openMenteri23 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri23.class);
+		startActivity(i);
+		}
+	public void openMenteri24 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri24.class);
+		startActivity(i);
+		}
+	public void openMenteri25 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri25.class);
+		startActivity(i);
+		}
+	public void openMenteri26 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri26.class);
+		startActivity(i);
+		}
+	public void openMenteri27 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri27.class);
+		startActivity(i);
+		}
+	public void openMenteri28 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri28.class);
+		startActivity(i);
+		}
+	public void openMenteri29 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri29.class);
+		startActivity(i);
+		}
+	public void openMenteri30 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri30.class);
+		startActivity(i);
+		}
+	public void openMenteri31 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri31.class);
+		startActivity(i);
+		}
+	public void openMenteri32 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri32.class);
+		startActivity(i);
+		}
+	public void openMenteri33 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri33.class);
+		startActivity(i);
+		}
+	public void openMenteri34 (View v){
+		Intent i = new Intent (getApplicationContext(),Menteri34.class);
+		startActivity(i);
+		}
+	
+}
---
+++ src/com/eritrikat/kabinetkerjajokowi_jk/Menteri23.java
@@ -0,0 +1,35 @@
+package com.eritrikat.kabinetkerjajokowi_jk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Menteri23 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menteri23);
+		setTitle(" Menteri Pemuda dan Olahraga");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menteri23, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
